9df181b47cc8bb7622085376d31d718f136b809d
xiashaohua951004 <xsh1285091764@163.com>
2018-08-30 14:38:05 +0800

    饿了吗

---
+++ src/vuex/store.js
@@ -0,0 +1,28 @@
+// import Vue from 'vue';
+// import Vuex from 'vuex';
+// import state from './state';
+// import mutations from './mutations';
+// import actions from './actions';
+// import getters from './getters';
+// Vue.use(Vuex);
+//
+// export default new Vuex.Store({
+//   state,
+//   mutations,
+//   actions,
+//   getters
+// });
+import Vue from 'vue';
+import Vuex from 'vuex';
+import state from './state';
+import mutations from './mutations';
+import getters from './getters';
+import actions from './actions';
+
+Vue.use(Vuex);
+export default new Vuex.Store({
+  state,
+  mutations,
+  getters,
+  actions
+});
